cbb2070f54ceab68cd2c4d8a833484c6352aedc1
SandraS <sandra.siedlecka@opoczta.pl>
2026-05-20 18:28:56 +0200

    Dodanie pętli FOR

--- paczki.py
+++ paczki.py
@@ -1,9 +1,17 @@
 l_elementow = 0
 liczba_paczek = 0
-#liczba_kilogramow= 0
+waga_elementu = list
 #waga_paczki = 0
 
 l_elementow = int(input("Podal ilość elementów: "))
-waga_elementu = input("Podaj wagę elementów separując je przecinkiem: ")
+for k in range (l_elementow):
+    print(f"Podaj wagę {k+1} elementu")
+    waga_elementu.insert(k,input()) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    #waga_elementu.append(int(input())) #= input("Podaj wagę elementów separując je przecinkiem: ")
 print(f"Ilość elementów do wysłania: {l_elementow}")
-print(f"Waga elementów: {waga_elementu.split(',')}")
+#print(f"Waga elementów: {waga_elementu.split(',')}")
+print(waga_elementu)
+#waga_paczki = 0
+#while waga_paczki <= 20:
+ #   liczba_paczek = 1
+  #  waga_paczki =
