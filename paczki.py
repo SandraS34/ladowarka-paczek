d5ba9a9e8c4a8f6144410d27ac65fdeac931e409
SandraS <sandra.siedlecka@opoczta.pl>
2026-05-25 21:48:51 +0200

    Działająca pętla for do wczytania elementów z warunkiem zakresu wagi

--- paczki.py
+++ paczki.py
@@ -1,16 +1,17 @@
 l_elementow = 0
 liczba_paczek = 0
-waga_elementu = list
+calkowita_waga_elementow = 0
 #waga_paczki = 0
 
-l_elementow = int(input("Podal ilość elementów: "))
-for k in range (l_elementow):
-    print(f"Podaj wagę {k+1} elementu")
-    waga_elementu.insert(k,input()) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    #waga_elementu.append(int(input())) #= input("Podaj wagę elementów separując je przecinkiem: ")
-print(f"Ilość elementów do wysłania: {l_elementow}")
-#print(f"Waga elementów: {waga_elementu.split(',')}")
-print(waga_elementu)
+l_elementow = int(input("Podal ilość elementów do wysłania: ")) #Podanie przez użytkownika liczbę elementów do wysłania
+for element in range (l_elementow):
+    waga_elementu = float(input(f"Podaj wagę {element+1} elementu: ")) #Pobranie informacji o wadze poszczególnych elementów
+    if waga_elementu in range (1, 10):                                  # Warunek, że waga elementu musi być z zakresu 1-10 kg
+        calkowita_waga_elementow += waga_elementu
+    else:
+        print("Waga elementu nie mieści się w zakresie 1-10 kg")
+
+print(f"Całkowita waga elementów wynosi {calkowita_waga_elementow} kg.")
 #waga_paczki = 0
 #while waga_paczki <= 20:
  #   liczba_paczek = 1
