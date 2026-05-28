9cb9a821eed2ffc737d355d965e045e9529fb861
SandraS <sandra.siedlecka@opoczta.pl>
2026-05-28 20:48:38 +0200

    Paprawiona wersja obliczania wagi najlżejzej paczki

--- paczki.py
+++ paczki.py
@@ -1,4 +1,4 @@
-l_elementow = 0
+#l_elementow = 0
 liczba_paczek = 1
 calkowita_waga_elementow = 0
 max_waga_paczki = 20
@@ -6,14 +6,14 @@ aktualna_waga_paczki = 0
 waga_najlzejszej_paczki = 0
 najlzejsza_paczka = 1
 
-l_elementow = int(input("Podal ilość elementów do wysłania: ")) #Podanie przez użytkownika liczbę elementów do wysłania
+l_elementow = int(input("Podaj ilość elementów do wysłania: ")) #Podanie przez użytkownika liczbę elementów do wysłania
 for element in range (l_elementow):
     waga_elementu = int(input(f"Podaj wagę {element+1} elementu: ")) #Pobranie informacji o wadze poszczególnych elementów
     if waga_elementu in range (1, 10):                                  # Warunek, że waga elementu musi być z zakresu 1-10 kg
         calkowita_waga_elementow += waga_elementu
         if waga_elementu + aktualna_waga_paczki <= max_waga_paczki:     # Warunek sprawdza czy następny element zmieści się do aktualnej paczki
             aktualna_waga_paczki += waga_elementu
-            waga_najlzejszej_paczki += aktualna_waga_paczki
+            waga_najlzejszej_paczki += waga_elementu
         else:
             aktualna_waga_paczki = waga_elementu
             liczba_paczek += 1
@@ -27,4 +27,4 @@ puste_kg = liczba_paczek * 20 - calkowita_waga_elementow
 print(f"Całkowita waga elementów wynosi {calkowita_waga_elementow} kg.")
 print(f"Ilość paczek do wysłania: {liczba_paczek}")
 print(f"Suma pustych kilogramów wynosi: {puste_kg} kg.")
-print(f"Najwięcej pustych kilogramów miała paczka nr. {najlzejsza_paczka}, z wynikiem {20 - waga_najlzejszej_paczki} kg.")
+print(f"Najwięcej pustych kilogramów miała paczka nr. {najlzejsza_paczka}, z wynikiem {max_waga_paczki - waga_najlzejszej_paczki} kg.")
